a2fcc176f4565c714e8a756329e81dee3610d466
Andrew Miller <ak.miller@auckland.ac.nz>
2010-02-03 13:57:36 +1300

    Fix script.

--- scripts/run-platform-command.py
+++ scripts/run-platform-command.py
@@ -77,7 +77,10 @@ options = sys.argv[3].split(',')
 
 def firstWithDefault(f, d, x):
     l = filter(f, d, x)
-    if len(l) == 0: d else: l.pop()
+    if len(l) == 0:
+        d
+    else:
+        l.pop()
 
 deptype = firstWithDefault(lambda x: x == "clobber" or x == "depend", "depend", options)
 testtype = firstWithDefault(lambda x: x == "rawcpu" or x == "valgrind", "rawcpu", options)
